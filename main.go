2ec05bb22ed26d0f2bee20de60054f0b609f721d
Abyss <timofeyfo@gmail.com>
2026-03-10 00:35:35 +0300

    Оно живое

--- main.go
+++ main.go
@@ -5,21 +5,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	_ "modernc.org/sqlite"
 )
 
 const (
-	schemaSQL = `CREATE TABLE IF NOT EXISTS payments (id INTEGER PRIMARY KEY, person TEXT, Amount float, date datetime);`
+	schemaSQL = `CREATE TABLE IF NOT EXISTS payments (Id INTEGER PRIMARY KEY, Person TEXT, Amount float, Date TEXT);`
 )
 
 var paymentDB *sql.DB
 
-const dbfile = "/payments.db"
+const dbfile = "payments.db"
 
 func NewDB(dbpath string) error {
-	paymentDB, err := sql.Open("sqlite", "payments.db")
+	var err error
+	paymentDB, err = sql.Open("sqlite", dbpath)
 	if err != nil {
 		return err
 	}
@@ -52,28 +54,29 @@ func main() {
 }
 
 type Payment struct {
-	Person string `json:"person"`
-	Amount int    `json:"amount"`
-	Time   string `json:"date"`
+	Id     int     `json:"id"`
+	Person string  `json:"Person"`
+	Amount float32 `json:"Amount"`
+	Date   string  `json:"Date"`
 }
 
 type Person struct {
-	Name string `json:"name"`
+	Name string `json:"Person"`
 }
 
 func paymentHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		fmt.Printf("Post called\n")
+		fmt.Printf("\nPost called\n")
 		postMethod(w, r)
 	case "GET":
-		fmt.Printf("Get called\n")
+		fmt.Printf("\nGet called\n")
 		getMethod(w, r)
-	case "UPDATE":
-		fmt.Printf("Update called\n")
+	case "PATCH":
+		fmt.Printf("\nUpdate called\n")
 		updateMethod(w, r)
 	case "DELETE":
-		fmt.Printf("Delete called\n")
+		fmt.Printf("\nDelete called\n")
 		deleteMethod(w, r)
 	default:
 		http.Error(w, "Invalid HTTP method", http.StatusMethodNotAllowed)
@@ -83,19 +86,29 @@ func paymentHandler(w http.ResponseWriter, r *http.Request) {
 
 func postMethod(w http.ResponseWriter, r *http.Request) {
 	payment := Payment{}
-	fmt.Printf("%+v", r.Body)
 	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
 		fmt.Print("error decoding payment")
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Printf(err.Error())
 		return
 	}
-	println(payment.Person, payment.Amount, payment.Time)
-	fmt.Printf("%+v", payment)
-	_, err := paymentDB.Exec("INSERT INTO payments (time, amount, person) VALUES ($1, $2, $3)", payment.Time, payment.Amount, payment.Person)
+
+	println("\n Unmarshaled payment")
+	println(payment.Person, payment.Amount, payment.Date)
+	println("---\n")
+
+	sql_q := `INSERT INTO payments (person, amount, date) VALUES (?, ?, ?)`
+
+	var result, err = paymentDB.Exec(sql_q, payment.Person, payment.Amount, payment.Date)
 	if err != nil {
+		println(err.Error())
 		return
 	}
+	println(result.LastInsertId())
 	w.WriteHeader(http.StatusCreated)
+	var id, _ = result.LastInsertId()
+	res := "Payment created with ID: " + strconv.FormatInt(id, 10)
+	w.Write([]byte(res))
 }
 
 func getMethod(w http.ResponseWriter, r *http.Request) {
@@ -105,15 +118,25 @@ func getMethod(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	println(person.Name)
+
 	result, err := paymentDB.Query("SELECT * FROM payments WHERE person = $1", person.Name)
 	if err != nil {
 		return
 	}
+	println(result)
 
 	var paymentsQuery Payment
 	var paymentsRes []Payment
 	for result.Next() {
-		result.Scan(&paymentsQuery)
+		var id int64
+		err := result.Scan(&paymentsQuery.Id, &paymentsQuery.Person, &paymentsQuery.Amount, &paymentsQuery.Date)
+		if err != nil {
+			println(err.Error())
+			return
+		}
+		println(id, paymentsQuery.Person, paymentsQuery.Amount, paymentsQuery.Date)
+		println("---")
 		paymentsRes = append(paymentsRes, paymentsQuery)
 	}
 
@@ -125,9 +148,45 @@ func getMethod(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMethod(w http.ResponseWriter, r *http.Request) {
+	payment := Payment{}
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		println(err.Error())
+		return
+	}
 
+	println(payment.Id)
+
+	result, err := paymentDB.Exec("UPDATE payments set Person = $2, Amount = $3, Date = $4 where id = $1", payment.Id, payment.Person, payment.Amount, payment.Date)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	rowsN, _ := result.RowsAffected()
+	res := "Updated rows: " + strconv.FormatInt(rowsN, 10)
+	w.Write([]byte("Payment updated successfully: " + res))
 }
 
 func deleteMethod(w http.ResponseWriter, r *http.Request) {
+	payment := Payment{}
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		println(err.Error())
+		return
+	}
 
+	println(payment.Id)
+
+	result, err := paymentDB.Exec("DELETE from payments where Id = $1", payment.Id)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	rowsN, _ := result.RowsAffected()
+	res := "Deleted rows: " + strconv.FormatInt(rowsN, 10)
+	w.Write([]byte("Payment deleted successfully: " + res))
 }
